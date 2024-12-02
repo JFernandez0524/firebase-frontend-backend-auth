f6a751f91ad2b481484a9277d8cf1c7fbc7759a5
JFernandez0524 <njphonefix@gmail.com>
2024-12-02 07:11:27 -0500

    updated .env in backend

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# Express Backend for Firbase auth APP
+
+will need to add Mongodb cluster password in .env file
+
+Server will run on Port 5000
--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 export async function connectDB() {
-  const uri = `mongodb+srv://badbank:josfer123@badbank.wc6srxt.mongodb.net/badBank?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://badbank:${process.env.MONGODB_PASSWORD}@badbank.wc6srxt.mongodb.net/badBank?retryWrites=true&w=majority`;
 
   try {
     // Connect to the MongoDB cluster
